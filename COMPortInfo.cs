db3bb8c8e897ed24891c26f34235f3d70d033e16
saintgene xu <saintgene@msn.com>
2015-11-04 18:25:19 -0500

    Added some backup xml files

---
+++ COMPortInfo.cs
@@ -0,0 +1,72 @@
+﻿//saintgene's class for ComPortInfo
+using System; 
+using System.Collections.Generic;
+using System.Management;
+
+namespace iSpyApplication
+{
+    internal class ProcessConnection
+    {
+
+        public static ConnectionOptions ProcessConnectionOptions()
+        {
+            ConnectionOptions options = new ConnectionOptions();
+            options.Impersonation = ImpersonationLevel.Impersonate;
+            options.Authentication = AuthenticationLevel.Default;
+            options.EnablePrivileges = true;
+            return options;
+        }
+
+        public static ManagementScope ConnectionScope(string machineName, ConnectionOptions options, string path)
+        {
+            ManagementScope connectScope = new ManagementScope();
+            connectScope.Path = new ManagementPath(@"\\" + machineName + path);
+            connectScope.Options = options;
+            connectScope.Connect();
+            return connectScope;
+        }
+    }
+
+    public class COMPortInfo
+    {
+        public uint iPort { get; set; }
+        public string Description { get; set; }
+
+        public COMPortInfo() { }
+
+        public static List<COMPortInfo> GetCOMPortsInfo()
+        {
+            List<COMPortInfo> comPortInfoList = new List<COMPortInfo>();
+
+            ConnectionOptions options = ProcessConnection.ProcessConnectionOptions();
+            ManagementScope connectionScope = ProcessConnection.ConnectionScope(Environment.MachineName, options, @"\root\CIMV2");
+
+            ObjectQuery objectQuery = new ObjectQuery("SELECT * FROM Win32_PnPEntity WHERE ConfigManagerErrorCode = 0");
+            ManagementObjectSearcher comPortSearcher = new ManagementObjectSearcher(connectionScope, objectQuery);
+
+            using (comPortSearcher)
+            {
+                string caption = null;
+                foreach (ManagementObject obj in comPortSearcher.Get())
+                {
+                    if (obj != null)
+                    {
+                        object captionObj = obj["Caption"];
+                        if (captionObj != null)
+                        {
+                            caption = captionObj.ToString();
+                            if (caption.Contains("(COM"))
+                            {
+                                COMPortInfo comPortInfo = new COMPortInfo();
+                                comPortInfo.iPort = uint.Parse(caption.Substring(caption.LastIndexOf("(COM")+4).TrimEnd(')'));
+                                comPortInfo.Description = caption;
+                                comPortInfoList.Add(comPortInfo);
+                            }
+                        }
+                    }
+                }
+            }
+            return comPortInfoList;
+        }
+    }
+}
